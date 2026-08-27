6b3217607bbfdb68048d8bc0a19ddf62d570eaa5
Juan-Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2026-08-27 16:43:43 -0300

    added eliminar method to ArbolBinario

--- proyecto-base-ut02/src/main/java/ucu/edu/aed/structures/ArbolBinario.java
+++ proyecto-base-ut02/src/main/java/ucu/edu/aed/structures/ArbolBinario.java
@@ -38,32 +38,130 @@ public class ArbolBinario<T> implements TDAArbolBinario<T> {
 
     @Override
     public boolean eliminar(Comparable<T> criterioBusqueda) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+        if (criterioBusqueda == null) {
+            throw new IllegalArgumentException(
+                    "criterioBusqueda en el metodo 'eliminar' es nulo");
+        }
+
+        if (this.raizEsNula()) {
+            return false;
+        }
+
+        int criterio = criterioBusqueda.compareTo(this.raiz.getDato());
+
+        // El nodo a eliminar es la raíz
+        if (criterio == 0) {
+            eliminarRaiz();
+            return true;
+        }
+
+        // El nodo a eliminar está debajo de la raíz
+        TDAElemento<T> nodoEliminado = this.raiz.eliminar(criterioBusqueda);
+
+        return nodoEliminado != null;
+    }
+
+    private void eliminarRaiz() {
+
+        TDAElemento<T> hijoIzquierdo = this.raiz.getHijoIzquierdo();
+        TDAElemento<T> hijoDerecho = this.raiz.getHijoDerecho();
+
+        // CASO 1: la raíz es una hoja
+        if (hijoIzquierdo == null && hijoDerecho == null) {
+            this.raiz = null;
+            return;
+        }
+
+        // CASO 2: solamente tiene hijo derecho
+        if (hijoIzquierdo == null) {
+            this.raiz = hijoDerecho;
+            return;
+        }
+
+        // CASO 3: solamente tiene hijo izquierdo
+        if (hijoDerecho == null) {
+            this.raiz = hijoIzquierdo;
+            return;
+        }
+
+        // CASO 4: tiene los dos hijos
+        TDAElemento<T> padreSucesor = this.raiz;
+        TDAElemento<T> sucesor = hijoDerecho;
+
+        // buscamos el menor del subárbol derecho
+        while (sucesor.getHijoIzquierdo() != null) {
+            padreSucesor = sucesor;
+            sucesor = sucesor.getHijoIzquierdo();
+        }
+
+        /*
+         * Si el sucesor no era directamente el hijo derecho
+         * de la raíz, lo sacamos de su posición original.
+         */
+        if (padreSucesor != this.raiz) {
+            padreSucesor.setHijoIzquierdo(
+                    sucesor.getHijoDerecho());
+
+            sucesor.setHijoDerecho(hijoDerecho);
+        }
+
+        sucesor.setHijoIzquierdo(hijoIzquierdo);
+
+        this.raiz = sucesor;
     }
 
     @Override
     public boolean insertar(Comparable<T> dato) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insertar'");
+        if (dato == null) {
+            throw new IllegalArgumentException(
+                    "dato en el metodo 'insertar' es nulo");
+        }
+
+        // si el arbol esta vacio
+        if (this.raizEsNula()) {
+            this.raiz = new Nodo<T>(convertirADato(dato));
+            return true;
+        }
+
+        // sino, delego al nodo
+        return this.raiz.insertar(dato);
     }
+
+    @SuppressWarnings("unchecked")
+    private T convertirADato(Comparable<T> dato) {
+        return (T) dato;
+    }
+
+    // son lo mismo, la de arriba se (me) hace mas facil de leer
+    // @Override
+    // public boolean insertar(Comparable<T> dato) {
+    // if (raizEsNula()) {
+    // this.raiz = new Nodo<T>((T) dato); // ???
+    // return true;
+    // } else {
+    // return raiz.insertar(dato);
+    // }
+    // }
 
     @Override
     public void inOrder(Consumer<T> consumidor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inOrder'");
+        if (!raizEsNula()) {
+            this.raiz.inOrder(elemento -> consumidor.accept(elemento.getDato()));
+        }
     }
 
     @Override
     public void preOrder(Consumer<T> consumidor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'preOrder'");
+        if (!raizEsNula()) {
+            this.raiz.preOrder(elemento -> consumidor.accept(elemento.getDato()));
+        }
     }
 
     @Override
     public void postOrder(Consumer<T> consumidor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'postOrder'");
+        if (!raizEsNula()) {
+            this.raiz.postOrder(elemento -> consumidor.accept(elemento.getDato()));
+        }
     }
 
     @Override
@@ -86,6 +184,7 @@ public class ArbolBinario<T> implements TDAArbolBinario<T> {
         return this.raizEsNula() ? 0 : this.raiz.cantidadNodosInternos();
     }
 
+    // yo se que no hace falta pero (me) ayuda a la lectura
     private boolean raizEsNula() {
         return this.raiz == null;
     }
